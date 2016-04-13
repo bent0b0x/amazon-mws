ba28133b969ea29b0ed0b99fee01ffb9c731588d
Benjamin Baum <bjb253@cornell.edu>
2016-04-13 10:59:22 -0500

    (chore) initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# amazon-mws-node
+Tiny library for interacting with the Amazon MWS API
+
+## Installation
+`$ npm install amazon-mws-node`
+
+## Usage
+```sh
+  var mws = require('amazon-mws-node')({
+    AmzSecretKey: 'AMZ_SECRET_KEY',
+    AWSAccessKeyId: 'AMZ_ACCESS_KEY_ID'
+  });
+
+  mws({
+    method: 'GET',
+    base: 'mws.amazonservices.com',
+    endpoint: '/Orders/2013-09-01',
+    params: {
+      'Action': 'ListOrders',
+      'CreatedAfter': '2014-01-01',
+      'MarketplaceId.Id.1': 'MARKETPLACE_ID',
+      'SellerId': 'SELLER_ID',
+      'MWSAuthToken': 'MWS_AUTH_TOKEN',
+      'Version': '2013-09-01'
+    },
+    callback: function (error, response, body) {
+      console.log(body);
+    }
+  });
+```
+
---
+++ amazon.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var request = require('request');
+var HmacSHA256 = require('crypto-js/hmac-sha256');
+var base64 = require('crypto-js/enc-base64');
+
+var initializeAmazon = function (initOptions) {
+  return function (options) {
+    var method = options.method || 'GET';
+    var url = 'https://';
+    url += options.base || 'mws.amazonservices.com';
+    url += options.endpoint;
+
+    options.params.AWSAccessKeyId = options.params.AWSAccessKeyId || initOptions.AWSAccessKeyId;
+    options.params.SignatureMethod = 'HmacSHA256';
+    options.params.SignatureVersion = '2';
+    options.params.Timestamp = new Date().toISOString();
+
+
+    var paramsArr = [];
+    for (param in options.params) {
+      paramsArr.push([param, options.params[param]]);
+    }
+    paramsArr.sort(function (a, b) {
+      return a[0] > b[0];
+    });
+
+    var keys = [];
+    var vals = [];
+    paramsArr.forEach(function (tuple) {
+      keys.push(encodeURIComponent(tuple[0]));
+      vals.push(encodeURIComponent(tuple[1]));
+    });
+
+    var paramsString = '';
+    keys.forEach(function (key, index) {
+      paramsString += key + '=' + vals[index];
+      if (index !== keys.length - 1) {
+        paramsString += '&';
+      }
+    });
+
+
+    var query = [method, options.base, options.endpoint, paramsString];
+    var queryString = query.join('\n');
+
+
+    var hmac = HmacSHA256(queryString, initOptions.AmzSecretKey);
+    var signature = base64.stringify(hmac);
+
+    paramsString += '&Signature=' + encodeURIComponent(signature);
+
+    url += '?' + paramsString;
+
+    request[method.toLowerCase()](url, function (error, response, body) {
+      options.callback && options.callback(error, response, body);
+    });
+  }
+};
+
+module.exports = initializeAmazon;
